07ea2d456a27e72af68999544dc7526db1e4377c
Abhishekkumar Hirdayram Choudhary <117569653+isAbhishekkumar@users.noreply.github.com>
2025-08-28 22:59:44 +0000

    fix

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -12,8 +12,7 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
-        jcenter() // Add JCenter for older dependencies
-        maven { url = uri("https://jitpack.io") } // Add JitPack for GitHub-hosted dependencies
+        maven { url = uri("https://jitpack.io") } 
     }
 }
 
